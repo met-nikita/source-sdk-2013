40e4e13c402277845f811ce37652f0272d152a74
Blixibon <acj3017@gmail.com>
2021-09-12 18:25:54 -0500

    Expanded automatic citizen EZ_VARIANT_ARBEIT to all E:Z2 citizen types with a global state to turn it off

--- sp/src/game/server/hl2/npc_citizen17.cpp
+++ sp/src/game/server/hl2/npc_citizen17.cpp
@@ -723,13 +723,15 @@ void CNPC_Citizen::Spawn()
 		// Randomize brute mask skin based on entity index
 		m_nSkin = entindex() % BRUTE_MASK_NUM_SKINS;
 	}
-	else if ( m_Type == CT_ARCTIC )
+	else if ( m_Type >= CT_BRUTE && m_Type <= CT_ARBEIT ) // CT_BRUTE, CT_LONGFALL, CT_ARCTIC, CT_ARBEIT
 	{
-		// For now, the "Arctic" citizen type should be equivalent to EZ_VARIANT_ARBEIT.
-		// This is mainly for dropping Arbeit items.
-		// (TODO: Might a mapper want arctic rebels to drop regular items? They could just use CT_UNIQUE, but they would lose random heads.)
-		if (m_tEzVariant == EZ_VARIANT_DEFAULT)
-			m_tEzVariant = EZ_VARIANT_ARBEIT;
+		if (GlobalEntity_GetState( "citizens_no_auto_variant" ) != GLOBAL_ON)
+		{
+			// By default, E:Z2's citizen types should be equivalent to EZ_VARIANT_ARBEIT.
+			// This is mainly for dropping Arbeit items.
+			if (m_tEzVariant == EZ_VARIANT_DEFAULT)
+				m_tEzVariant = EZ_VARIANT_ARBEIT;
+		}
 	}
 #endif
 	m_flTimePlayerStare = FLT_MAX;
